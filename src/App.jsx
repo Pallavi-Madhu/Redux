637b208b507a8844f3fac579c04744a3b8aefae3
Pallavi-Madhu-X <pallavimadhu30@gmail.com>
2025-03-04 07:05:18 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Product from './components/Product'
+
+function App() {
+  return (
+    <>
+      <Product/>
+    </>   
+  )
+}
+
+export default App
---
+++ src/components/Product.js
@@ -0,0 +1,8 @@
+const Product = () => {
+    return(
+        <>
+            <h2>Product dashboard</h2>
+        </>
+    )
+}
+export default Product;
