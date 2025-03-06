5f599a2162104c6d2de359eeb71655f164b04c09
Pallavi-Madhu-X <pallavimadhu30@gmail.com>
2025-03-06 07:02:38 +0530

    second edit in dashboard

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -10,16 +10,21 @@ const Product = () => {
     },[]);
 
     const cards = products.map((product) => (
-      <div className="col-md-3">
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+      <div className="col-md-3" style={{ marginBottom: "10px" }}>
+        <Card key={product.id} className="h-100">
+          <div className="text-center">
+            <Card.Img
+              variant="top"
+              src={product.image}
+              style={{ width: "100px", height: "150px" }}
+            ></Card.Img>
+          </div>
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Title>{product.title}</Card.Title>
+            <Card.Text>INR:{product.price}</Card.Text>
+            <Card.Footer>
+              <Button variant="primary">Add to cart</Button>
+            </Card.Footer>
           </Card.Body>
         </Card>
       </div>
