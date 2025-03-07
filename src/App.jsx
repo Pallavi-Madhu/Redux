ddc054ac02a0a40ef39e109d19b6d4ceee11c88e
Pallavi-Madhu-X <pallavimadhu30@gmail.com>
2025-03-07 07:06:21 +0530

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
+import Dashboard from "./components/Dashboard";
 import Product from "./components/Product";
+import RootLayout from "./components/RootLayout";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Cart from "./components/Cart";
+import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from "react-router-dom";
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout/>}>
+        <Route index element={<Dashboard />}></Route>
+        <Route path="cart" element={<Cart />}></Route>
+      </Route>
+    )
+  );
   return (
     <>
-      <Product />
+    <div className="App">
+      <RouterProvider router={router}/>
+    </div>
     </>
   );
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,41 +1,39 @@
-import { useState,useEffect } from "react";
-import { Card,Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Card, Button } from "react-bootstrap";
 const Product = () => {
-    const[products,getProducts] = useState([]);
-    useEffect(() => {
-        //api
-        fetch("https://fakestoreapi.com/products")
-        .then(data => data.json())
-        .then(result => getProducts(result))
-    },[]);
+  const [products, getProducts] = useState([]);
+  useEffect(() => {
+    //api
+    fetch("https://fakestoreapi.com/products")
+      .then((data) => data.json())
+      .then((result) => getProducts(result));
+  }, []);
 
-    const cards = products.map((product) => (
-      <div className="col-md-3" style={{ marginBottom: "10px" }}>
-        <Card key={product.id} className="h-100">
-          <div className="text-center">
-            <Card.Img
-              variant="top"
-              src={product.image}
-              style={{ width: "100px", height: "150px" }}
-            ></Card.Img>
-          </div>
-          <Card.Body>
-            <Card.Title>{product.title}</Card.Title>
-            <Card.Text>INR:{product.price}</Card.Text>
-            <Card.Footer>
-              <Button variant="primary">Add to cart</Button>
-            </Card.Footer>
-          </Card.Body>
-        </Card>
-      </div>
-    ));
-    return(
-        <>
-            <h2>Product dashboard</h2>
-            <div className="row">
-                {cards}
-            </div>
-        </>
-    )
-}
+  const cards = products.map((product) => (
+    <div className="col-md-3" style={{ marginBottom: "10px" }}>
+      <Card key={product.id} className="h-100">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{ width: "100px", height: "150px" }}
+          ></Card.Img>
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>INR:{product.price}</Card.Text>
+          <Card.Footer style={{backgroundColor:"white"}}>
+            <Button variant="primary">Add to cart</Button>
+          </Card.Footer>
+        </Card.Body>
+      </Card>
+    </div>
+  ));
+  return (
+    <>
+      <h2>Product dashboard</h2>
+      <div className="row">{cards}</div>
+    </>
+  );
+};
 export default Product;
